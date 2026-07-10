154a6e18294bb9d1fa00dd87052ba739ab529984
Zheng Kai <zhengkai@notdns.me>
2026-07-10 12:13:53 +0800

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,10 @@
+import { neon } from "@neondatabase/serverless";
+import { env } from "@repo/env/server";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import * as schema from "./schema";
+
+export function createDb() {
+  const sql = neon(env.DATABASE_URL || "");
+  return drizzle(sql, { schema });
+}
